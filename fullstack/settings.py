29ec9c3ea39fb28ecf4a48074382a1e1834ec64a
i-love-thinking <38521343+i-love-thinking@users.noreply.github.com>
2021-01-24 15:38:00 +0800

    Update settings.py
    update SECRET_KEY

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(o^2$#4318b%=tw0^j!)5d2n0(53ry*p!n^qbs=hlsh=((p=lt'
+SECRET_KEY = 'Your Secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
